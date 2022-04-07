e133fd931f6b4de4009eb11c287d1ac7eff910e3
Abdul Kader <abdulkader.69k@gmail.com>
2022-04-07 15:26:08 +0600

    lock popup design fixed

--- src/components/footer/modals/lockSettingsModal.tsx
+++ src/components/footer/modals/lockSettingsModal.tsx
@@ -273,7 +273,7 @@ const LockSettingsModal = () => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <div className="inline-block w-full h-96 max-w-lg p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                <div className="inline-block w-full max-w-lg p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
                   <button
                     className="close-btn absolute top-8 right-6 w-[25px] h-[25px] outline-none"
                     type="button"
