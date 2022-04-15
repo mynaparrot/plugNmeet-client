35fe12700a84d1a1e0f39b717d2303493c279c08
jibon Costa <jiboncosta57@gmail.com>
2022-04-15 23:09:10 +0600

    missed adding css for border-secondaryColor

--- src/components/extra-pages/Error.tsx
+++ src/components/extra-pages/Error.tsx
@@ -16,9 +16,9 @@ const ErrorPage = ({ title, text }: IErrorPageProps) => {
       <div
         className={`error-app-bg absolute w-full h-full left-0 top-0 object-cover pointer-events-none `}
         style={{
-          backgroundImage: `url("${`${assetPath}/imgs/app-banner.jpg`}")`,
+          backgroundImage: `url("${assetPath}/imgs/app-banner.jpg")`,
         }}
-      ></div>
+      />
       <div className="content relative z-20 text-center bg-white rounded-xl shadow-lg h-80 w-[450px] flex items-center justify-center">
         <div className="inner text-center">
           <h2 className="text-3xl font-medium mb-4">{title}</h2>
