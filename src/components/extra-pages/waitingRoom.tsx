431fc819db9299e6cc219fa641dcccbc6b886a0a
Abdul Kader <abdulkader.69k@gmail.com>
2022-05-19 00:08:18 +0600

    design update

--- src/components/extra-pages/waitingRoom.tsx
+++ src/components/extra-pages/waitingRoom.tsx
@@ -9,10 +9,10 @@ const WaitingRoom = () => {
     <>
       <div className="guest-lobby flex items-center justify-center w-full h-screen">
         <div className="guest-lobby-inner">
-          <div className="logo w-20 m-auto relative z-20">
+          <div className="logo w-40 m-auto relative z-20">
             <div
               className={`${
-                (window as any).CUSTOM_LOGO ? 'h-[45px]' : 'h-[45px]'
+                (window as any).CUSTOM_LOGO ? 'h-[90px]' : 'h-[90px]'
               } header-logo h-full bg-contain bg-no-repeat`}
               style={{
                 backgroundImage: `url("${logo}")`,
@@ -20,7 +20,6 @@ const WaitingRoom = () => {
             />
           </div>
           <div className="divider my-5 h-[2px] w-full max-w-[50px] bg-primaryColor m-auto"></div>
-          <h2 className="headline text-2xl text-center">Guest Lobby</h2>
           <div className="loading-wrap relative h-24">
             <div className="loading absolute text-center top-3 z-[999] left-0 right-0 m-auto">
               <div className="lds-ripple">
@@ -29,7 +28,7 @@ const WaitingRoom = () => {
               </div>
             </div>
           </div>
-          <p>{t('notifications.waiting-for-approval')}</p>
+          <p className="text-3xl">{t('notifications.waiting-for-approval')}</p>
         </div>
       </div>
     </>
