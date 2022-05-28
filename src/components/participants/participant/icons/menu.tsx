717e5df9d0d4eb029c297a402ad0d1653174a0b7
Abdul Kader <abdulkader.69k@gmail.com>
2022-05-28 22:13:41 +0600

    z-index issue fix

--- src/components/participants/participant/icons/menu.tsx
+++ src/components/participants/participant/icons/menu.tsx
@@ -29,7 +29,7 @@ const MenuIcon = ({ userId, openRemoveParticipantAlert }: IMenuIconProps) => {
                 show={open}
                 enter="transition duration-100 ease-out"
                 enterFrom="transform scale-95 opacity-0"
-                enterTo="transform scale-100 opacity-100"
+                enterTo="transform scale-100 opacity-100 z-10"
                 leave="transition duration-75 ease-out"
                 leaveFrom="transform scale-100 opacity-100"
                 leaveTo="transform scale-95 opacity-0"
