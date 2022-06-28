abd2a032e2d8d2775636acab3fcadade6b6f9435
jibon Costa <jiboncosta57@gmail.com>
2022-06-28 11:18:45 +0600

    fixed: local videos wasn't playing perfectly. Now we're using `videojs` player for direct video or audio file links.

---
+++ src/components/external-media-player/video-js/player.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import videojs, { VideoJsPlayer, VideoJsPlayerOptions } from 'video.js';
+import 'video.js/dist/video-js.css';
+
+interface IPlayerComponentProps {
+  options: VideoJsPlayerOptions;
+  onReady: (player: VideoJsPlayer) => void;
+}
+
+const PlayerComponent = ({ options, onReady }: IPlayerComponentProps) => {
+  const videoRef = React.useRef(null);
+  const [player, setPlayer] = useState<VideoJsPlayer>();
+
+  useEffect(() => {
+    const videoElement = videoRef.current;
+    if (videoElement && !player) {
+      const player = videojs(videoElement, options, () => {
+        onReady && onReady(player);
+      });
+      setPlayer(player);
+    }
+    //eslint-disable-next-line
+  }, [videoRef]);
+
+  // Dispose the Video.js player when the functional component unmounts
+  useEffect(() => {
+    return () => {
+      if (player) {
+        player.dispose();
+        setPlayer(undefined);
+      }
+    };
+  }, [player]);
+
+  return (
+    <div data-vjs-player>
+      <video ref={videoRef} className="video-js" />
+    </div>
+  );
+};
+
+export default PlayerComponent;
