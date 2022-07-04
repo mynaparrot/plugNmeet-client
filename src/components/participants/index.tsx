e96502e3cc77e98d1e891af2c38dec0e530289b1
jibon Costa <jiboncosta57@gmail.com>
2022-07-04 12:52:11 +0600

    rewritten `ParticipantsComponent`

--- src/components/participants/index.tsx
+++ src/components/participants/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Room } from 'livekit-client';
 
@@ -15,16 +15,26 @@ const ParticipantsComponent = ({
 }: IParticipantsComponentProps) => {
   const participants = useAppSelector(participantsSelector.selectAll);
   const { t } = useTranslation();
+  const [participantElms, setParticipantElms] = useState<JSX.Element[]>([]);
 
-  const renderParticipants = () => {
-    const session = store.getState().session;
-    const currentUserUserId = session.currentUser?.userId;
-    const allow_view_other_users_list =
-      session.currentRoom.metadata?.room_features
-        ?.allow_view_other_users_list ?? false;
-    const currentIsAdmin = session.currentUser?.metadata?.is_admin ?? false;
+  const session = store.getState().session;
+  const currentUserUserId = session.currentUser?.userId;
+  const allow_view_other_users_list =
+    session.currentRoom.metadata?.room_features?.allow_view_other_users_list ??
+    false;
+  const currentIsAdmin = session.currentUser?.metadata?.is_admin ?? false;
 
-    return participants.map((participant) => {
+  useEffect(() => {
+    const tmp = participants.filter(
+      (p) =>
+        p.name !== '' && p.userId !== 'RECORDER_BOT' && p.userId !== 'RTMP_BOT',
+    );
+    if (!tmp.length) {
+      return;
+    }
+
+    const elms: JSX.Element[] = [];
+    tmp.forEach((participant) => {
       const remoteParticipant = currentRoom.participants.get(participant.sid);
       if (!currentIsAdmin && !allow_view_other_users_list) {
         if (
@@ -34,37 +44,41 @@ const ParticipantsComponent = ({
           return;
         }
       }
-      // by default recorder won't be listed as it's hidden user
-      // still we'll use extra layer for checking.
-      if (
-        participant.userId === 'RECORDER_BOT' ||
-        participant.userId === 'RTMP_BOT'
-      ) {
-        return;
-      }
-      return (
+      elms.push(
         <ParticipantComponent
           key={participant.sid}
           participant={participant}
           remoteParticipant={remoteParticipant}
-        />
+        />,
       );
     });
+
+    if (elms.length) {
+      setParticipantElms(elms);
+    }
+    //eslint-disable-next-line
+  }, [participants]);
+
+  const render = () => {
+    if (!participantElms.length) {
+      return null;
+    }
+    return (
+      <div className="inner-wrapper relative z-20">
+        <div className="top flex items-center justify-between font-medium mb-3 xl:mb-5">
+          <p className="text-sm text-black">
+            {t('left-panel.participants', {
+              total: participantElms.length,
+            })}
+          </p>
+        </div>
+
+        <div className="all-participants-wrap">{participantElms}</div>
+      </div>
+    );
   };
 
-  return (
-    <div className="inner-wrapper relative z-20">
-      <div className="top flex items-center justify-between font-medium mb-3 xl:mb-5">
-        <p className="text-sm text-black">
-          {t('left-panel.participants', {
-            total: renderParticipants().length,
-          })}
-        </p>
-      </div>
-
-      <div className="all-participants-wrap">{renderParticipants()}</div>
-    </div>
-  );
+  return render();
 };
 
 export default ParticipantsComponent;
