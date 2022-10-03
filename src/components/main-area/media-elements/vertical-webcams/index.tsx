de4f91f89e372ac076a2766bf41a23d7bc0ba3cb
Jibon L. Costa <jiboncosta57@gmail.com>
2022-10-03 13:28:32 +0600

    removed by mistake

--- src/components/main-area/media-elements/vertical-webcams/index.tsx
+++ src/components/main-area/media-elements/vertical-webcams/index.tsx
@@ -1,14 +1,21 @@
 import React, { useMemo } from 'react';
+import { createSelector } from '@reduxjs/toolkit';
 import { LocalParticipant, RemoteParticipant } from 'livekit-client';
 
+import { RootState, useAppSelector } from '../../../../store';
 import VideoElements from '../videos';
 
 interface IVerticalWebcamsProps {
   videoSubscribers?: Map<string, LocalParticipant | RemoteParticipant>;
 }
-
+const activateWebcamsViewSelector = createSelector(
+  (state: RootState) => state.roomSettings.activateWebcamsView,
+  (activateWebcamsView) => activateWebcamsView,
+);
 const VerticalWebcams = ({ videoSubscribers }: IVerticalWebcamsProps) => {
-  return useMemo(() => {
+  const activateWebcamsView = useAppSelector(activateWebcamsViewSelector);
+
+  const renderElms = useMemo(() => {
     if (!videoSubscribers) {
       return null;
     }
@@ -20,6 +27,8 @@ const VerticalWebcams = ({ videoSubscribers }: IVerticalWebcamsProps) => {
       />
     );
   }, [videoSubscribers]);
+
+  return activateWebcamsView ? renderElms : null;
 };
 
 export default VerticalWebcams;
