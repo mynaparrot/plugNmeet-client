6da9406739cf33e9adab4c393397614a029408bf
Jibon L. Costa <jiboncosta57@gmail.com>
2022-10-03 12:19:01 +0600

    check from participants' list before show as active speaker

--- src/components/main-area/active-speakers/index.tsx
+++ src/components/main-area/active-speakers/index.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
-import { useAppSelector } from '../../../store';
+import { store, useAppSelector } from '../../../store';
 import { activeSpeakersSelector } from '../../../store/slices/activeSpeakersSlice';
 import SpeakerComponent from './speaker';
 
 const ActiveSpeakers = () => {
   const activeSpeakers = useAppSelector(activeSpeakersSelector.selectAll);
 
-  const render = () => {
+  const activeSpeakersElms = useMemo(() => {
+    if (!activeSpeakers.length) {
+      return null;
+    }
+    const participantIds = store.getState().participants.ids;
     return activeSpeakers.map((speaker) => {
-      return <SpeakerComponent key={speaker.sid} speaker={speaker} />;
+      if (participantIds.find((p) => p === String(speaker.userId))) {
+        return <SpeakerComponent key={speaker.sid} speaker={speaker} />;
+      }
     });
-  };
+  }, [activeSpeakers]);
 
-  return (
+  return activeSpeakersElms ? (
     <div className="active-speakers-wrap flex w-full items-center justify-center absolute top-0 left-0 z-[9999]">
-      {activeSpeakers.length ? render() : null}
+      {activeSpeakersElms}
     </div>
-  );
+  ) : null;
 };
 
-export default React.memo(ActiveSpeakers);
+export default ActiveSpeakers;
