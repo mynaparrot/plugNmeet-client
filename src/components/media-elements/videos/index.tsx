7628368f0f39e9a020d7aa37590e446094d74d3b
Jibon L. Costa <jiboncosta57@gmail.com>
2022-10-05 12:34:23 +0600

    moved to own directory

--- src/components/media-elements/videos/index.tsx
+++ src/components/media-elements/videos/index.tsx
@@ -1,239 +1,62 @@
 import React, { useEffect, useMemo, useState } from 'react';
+import {
+  CurrentConnectionEvents,
+  IConnectLivekit,
+} from '../../../helpers/livekit/types';
 import { LocalParticipant, RemoteParticipant } from 'livekit-client';
-import { useTranslation } from 'react-i18next';
 import { createSelector } from '@reduxjs/toolkit';
 
-import {
-  RootState,
-  store,
-  useAppDispatch,
-  useAppSelector,
-} from '../../../store';
-import { setWebcamPaginating } from '../../../store/slices/sessionSlice';
-import useVideoParticipant from './helpers/useVideoParticipant';
-import { UserDeviceType } from '../../../store/slices/interfaces/session';
-import {
-  setForMobileAndTablet,
-  setForMobileLandscape,
-  setForPC,
-} from './helpers/utils';
+import MainVideoView from './main-video-view';
+import VerticalVideoView from './vertical-video-view';
 
-interface IVideoElementsProps {
-  videoSubscribers: Map<string, LocalParticipant | RemoteParticipant>;
-  perPage?: number;
+import { RootState, useAppSelector } from '../../../store';
+
+interface IVideosComponentProps {
+  currentConnection?: IConnectLivekit;
   isVertical?: boolean;
 }
-export interface VideoParticipantType {
-  isAdmin: boolean;
-  isLocal: boolean;
-}
 
-const MOBILE_PER_PAGE = 6,
-  TABLET_PER_PAGE = 9,
-  DESKTOP_PER_PAGE = 24;
-
-const screenWidthSelector = createSelector(
-  (state: RootState) => state.bottomIconsActivity.screenWidth,
-  (screenWidth) => screenWidth,
-);
-const deviceOrientationSelector = createSelector(
-  (state: RootState) => state.bottomIconsActivity.deviceOrientation,
-  (deviceOrientation) => deviceOrientation,
+const activateWebcamsViewSelector = createSelector(
+  (state: RootState) => state.roomSettings.activateWebcamsView,
+  (activateWebcamsView) => activateWebcamsView,
 );
 
-const VideoElements = ({
-  videoSubscribers,
-  perPage,
+const VideosComponent = ({
+  currentConnection,
   isVertical,
-}: IVideoElementsProps) => {
-  const dispatch = useAppDispatch();
-  const { t } = useTranslation();
-  const { allParticipants, totalNumWebcams } =
-    useVideoParticipant(videoSubscribers);
-  const screenWidth = useAppSelector(screenWidthSelector);
-  const deviceOrientation = useAppSelector(deviceOrientationSelector);
-  const deviceType = store.getState().session.userDeviceType;
+}: IVideosComponentProps) => {
+  const activateWebcamsView = useAppSelector(activateWebcamsViewSelector);
+  const [videoSubscribers, setVideoSubscribers] =
+    useState<Map<string, LocalParticipant | RemoteParticipant>>();
 
-  const [currentPage, setCurrentPage] = useState<number>(0);
-  const [participantsToRender, setParticipantsToRender] = useState<
-    Array<JSX.Element>
-  >([]);
-  const [showPre, setShowPre] = useState<boolean>(false);
-  const [showNext, setShowNext] = useState<boolean>(false);
-  const [webcamPerPage, setWebcamPerPage] = useState<number>(
-    perPage ?? DESKTOP_PER_PAGE,
-  );
-  const [isSmallScreen, setIsSmallScreen] = useState<boolean>(false);
-
-  const setParticipantsToDisplay = (
-    [...allParticipants]: Array<JSX.Element>,
-    page_number: number,
-    per_page: number,
-  ) => {
-    const display = allParticipants.slice(
-      (page_number - 1) * per_page,
-      page_number * per_page,
+  useEffect(() => {
+    if (currentConnection?.videoSubscribersMap.size) {
+      setVideoSubscribers(currentConnection.videoSubscribersMap as any);
+    }
+    currentConnection?.on(
+      CurrentConnectionEvents.VideoSubscribers,
+      setVideoSubscribers,
     );
+    return () => {
+      currentConnection?.off(
+        CurrentConnectionEvents.VideoSubscribers,
+        setVideoSubscribers,
+      );
+    };
+  }, [currentConnection]);
 
-    setParticipantsToRender(display);
-    if (page_number === 1) {
-      dispatch(setWebcamPaginating(false));
-    } else {
-      dispatch(setWebcamPaginating(true));
+  const videoSubscriberElms = useMemo(() => {
+    if (!videoSubscribers?.size) {
+      return null;
     }
-  };
-
-  useMemo(() => {
-    if (allParticipants.length <= webcamPerPage) {
-      // we don't need any pagination
-      setParticipantsToRender(allParticipants);
-    } else {
-      setCurrentPage(1);
-      setParticipantsToDisplay(allParticipants, 1, webcamPerPage);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [allParticipants, webcamPerPage]);
-
-  useEffect(() => {
-    if (totalNumWebcams > webcamPerPage) {
-      if (currentPage === 1) {
-        setShowPre(false);
-      } else {
-        setShowPre(true);
-      }
-
-      if (currentPage >= totalNumWebcams / webcamPerPage) {
-        setShowNext(false);
-      } else {
-        setShowNext(true);
-      }
-    } else {
-      setShowPre(false);
-      setShowNext(false);
-    }
-  }, [totalNumWebcams, currentPage, webcamPerPage]);
-
-  useEffect(() => {
-    if (!screenWidth || isVertical) {
-      return;
-    }
-    if (screenWidth <= 640) {
-      setWebcamPerPage(MOBILE_PER_PAGE);
-      setIsSmallScreen(true);
-    } else if (screenWidth > 640 && screenWidth <= 1024) {
-      if (deviceType === UserDeviceType.MOBILE) {
-        setWebcamPerPage(MOBILE_PER_PAGE);
-      } else {
-        setWebcamPerPage(TABLET_PER_PAGE);
-      }
-      setIsSmallScreen(true);
-    } else {
-      if (deviceType === UserDeviceType.TABLET) {
-        setWebcamPerPage(TABLET_PER_PAGE);
-        setIsSmallScreen(true);
-      } else {
-        setWebcamPerPage(DESKTOP_PER_PAGE);
-        setIsSmallScreen(false);
-      }
-    }
-    //eslint-disable-next-line
-  }, [screenWidth, isVertical]);
-
-  const prePage = () => {
-    setParticipantsToDisplay(allParticipants, currentPage - 1, webcamPerPage);
-    setCurrentPage(currentPage - 1);
-  };
-
-  const nextPage = () => {
-    setParticipantsToDisplay(allParticipants, currentPage + 1, webcamPerPage);
-    setCurrentPage(currentPage + 1);
-  };
-
-  const videoParticipantsElms = useMemo(() => {
     if (isVertical) {
-      return participantsToRender;
-    }
-    let elms: Array<JSX.Element>;
-
-    if (
-      deviceType === UserDeviceType.MOBILE &&
-      deviceOrientation === 'landscape'
-    ) {
-      elms = setForMobileLandscape(participantsToRender);
-    } else if (
-      deviceType === UserDeviceType.MOBILE ||
-      deviceType === UserDeviceType.TABLET ||
-      isSmallScreen
-    ) {
-      // for mobile & tablet
-      // at present we can use same logic for small screen of desktop
-      elms = setForMobileAndTablet(participantsToRender);
+      return <VerticalVideoView videoSubscribers={videoSubscribers} />;
     } else {
-      // for PC
-      elms = setForPC(participantsToRender);
+      return <MainVideoView videoSubscribers={videoSubscribers} />;
     }
+  }, [videoSubscribers, isVertical]);
 
-    return elms;
-    //eslint-disable-next-line
-  }, [participantsToRender, deviceOrientation]);
-
-  const render = () => {
-    return (
-      <>
-        {showPre ? (
-          <button
-            type="button"
-            className="previous-cam"
-            onClick={() => prePage()}
-          >
-            <i className="pnm-arrow-up" />
-          </button>
-        ) : null}
-
-        {/*all webcams*/}
-        <>{videoParticipantsElms}</>
-
-        {showNext ? (
-          <button type="button" className="next-cam" onClick={() => nextPage()}>
-            <i className="pnm-arrow-down" />
-          </button>
-        ) : null}
-      </>
-    );
-  };
-
-  return (
-    <>
-      {totalNumWebcams > 0 ? (
-        <div
-          className={`all-webcam-wrapper total-cam-${totalNumWebcams} selected-cam-${webcamPerPage} page-${currentPage} ${
-            isVertical ? 'vertical-webcams' : ''
-          }`}
-        >
-          <div className="all-webcam-wrapper-inner">{render()}</div>
-        </div>
-      ) : null}
-      {deviceType === UserDeviceType.DESKTOP &&
-      totalNumWebcams > 6 &&
-      !isVertical ? (
-        <div className="select-camera-number">
-          <label htmlFor="select-camera-num">{t('app.webcams-per-page')}</label>
-          <select
-            name="select-camera-num"
-            id="select-camera-num"
-            onChange={(e) => setWebcamPerPage(Number(e.currentTarget.value))}
-          >
-            <option value="6">6</option>
-            <option value="8">8</option>
-            <option value="12">12</option>
-            <option value="15">15</option>
-            <option value="18">18</option>
-            <option selected>24</option>
-          </select>
-        </div>
-      ) : null}
-    </>
-  );
+  return <>{activateWebcamsView ? videoSubscriberElms : null}</>;
 };
 
-export default VideoElements;
+export default VideosComponent;
