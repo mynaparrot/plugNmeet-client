91113dfb5f05ca2ca428f4c3a13207127cb8eee8
Jibon L. Costa <jiboncosta57@gmail.com>
2022-10-06 12:00:14 +0600

    improve VideosComponent

--- src/components/media-elements/videos/index.tsx
+++ src/components/media-elements/videos/index.tsx
@@ -1,15 +1,23 @@
-import React, { useMemo } from 'react';
-import { IConnectLivekit } from '../../../helpers/livekit/types';
+import React, { useEffect, useMemo, useState } from 'react';
+import { LocalParticipant, RemoteParticipant, Track } from 'livekit-client';
 import { createSelector } from '@reduxjs/toolkit';
+import { concat, isEmpty } from 'lodash';
 
+import VideosComponentElms, {
+  VideoParticipantType,
+} from './videosComponentElms';
+import VideoParticipant from './videoParticipant';
+import {
+  CurrentConnectionEvents,
+  IConnectLivekit,
+} from '../../../helpers/livekit/types';
 import { RootState, useAppSelector } from '../../../store';
-import VideosComponentElms from './videosComponentElms';
+import { ICurrentUserMetadata } from '../../../store/slices/interfaces/session';
 
 interface IVideosComponentProps {
   currentConnection: IConnectLivekit;
   isVertical?: boolean;
 }
-
 const activateWebcamsViewSelector = createSelector(
   (state: RootState) => state.roomSettings.activateWebcamsView,
   (activateWebcamsView) => activateWebcamsView,
@@ -20,16 +28,102 @@ const VideosComponent = ({
   isVertical,
 }: IVideosComponentProps) => {
   const activateWebcamsView = useAppSelector(activateWebcamsViewSelector);
+  const [videoSubscribers, setVideoSubscribers] =
+    useState<Map<string, LocalParticipant | RemoteParticipant>>();
+  const [allParticipants, setAllParticipants] = useState<JSX.Element[]>([]);
+  const [totalNumWebcams, setTotalNumWebcams] = useState<number>(0);
+
+  useEffect(() => {
+    if (currentConnection.videoSubscribersMap.size) {
+      setVideoSubscribers(currentConnection.videoSubscribersMap as any);
+    }
+    currentConnection.on(
+      CurrentConnectionEvents.VideoSubscribers,
+      setVideoSubscribers,
+    );
+    return () => {
+      currentConnection.off(
+        CurrentConnectionEvents.VideoSubscribers,
+        setVideoSubscribers,
+      );
+    };
+  }, [currentConnection]);
+
+  useMemo(() => {
+    if (!videoSubscribers) {
+      return;
+    } else if (!videoSubscribers.size) {
+      setTotalNumWebcams(0);
+      setAllParticipants([]);
+    }
+
+    let totalNumWebcams = 0;
+    const localSubscribers: Array<JSX.Element> = [];
+    const adminSubscribers: Array<JSX.Element> = [];
+    const otherSubscribers: Array<JSX.Element> = [];
+
+    videoSubscribers.forEach((participant) => {
+      // we will only take if source from Camera
+      const videoTracks = participant
+        .getTracks()
+        .filter((track) => track.source === Track.Source.Camera);
+
+      if (videoTracks.length) {
+        let isAdmin = false;
+        if (participant.metadata && !isEmpty(participant.metadata)) {
+          const metadata: ICurrentUserMetadata = JSON.parse(
+            participant.metadata,
+          );
+          isAdmin = metadata.is_admin;
+        }
+
+        const participantType: VideoParticipantType = {
+          isAdmin,
+          isLocal: participant instanceof LocalParticipant,
+        };
+
+        totalNumWebcams = totalNumWebcams + videoTracks.length;
+        const elm = (
+          <VideoParticipant
+            key={participant.sid}
+            participantType={participantType}
+            participant={participant}
+          />
+        );
+
+        if (isAdmin) {
+          adminSubscribers.push(elm);
+        } else {
+          if (participant instanceof LocalParticipant) {
+            localSubscribers.push(elm);
+          } else {
+            otherSubscribers.push(elm);
+          }
+        }
+      }
+    });
+
+    const allParticipants = concat(
+      adminSubscribers,
+      localSubscribers,
+      otherSubscribers,
+    );
+
+    setTotalNumWebcams(totalNumWebcams);
+    setAllParticipants(allParticipants);
+  }, [videoSubscribers]);
 
   const videoSubscriberElms = useMemo(() => {
     return (
       <VideosComponentElms
-        currentConnection={currentConnection}
+        allParticipants={allParticipants}
+        totalNumWebcams={totalNumWebcams}
         perPage={isVertical ? 3 : undefined}
         isVertical={isVertical}
       />
     );
-  }, [currentConnection, isVertical]);
+    //eslint-disable-next-line
+  }, [allParticipants, isVertical]);
 
   return <>{activateWebcamsView ? videoSubscriberElms : null}</>;
 };
