4a04fde4d1e9a82366ed045bfb3a8f2c59bdd51d
Abdul Kader <abdulkader.69k@gmail.com>
2023-07-24 17:41:20 +0600

    small note pad and hide icon added.

--- src/components/shared-notepad/index.tsx
+++ src/components/shared-notepad/index.tsx
@@ -67,13 +67,16 @@ const SharedNotepadElement = () => {
   const render = () => {
     if (url) {
       return (
-        <div className="h-[calc(100%-50px)] sm:px-5 mt-9">
+        <div className="h-[calc(100%-50px)] sm:px-5 mt-9 flex">
           <Draggable handle="#draggable-h1">
-            <div className="notepad-wrapper h-[calc(100%-80px)] w-full max-w-[1100px] m-auto cursor-pointer relative">
+            <div className="notepad-wrapper h-[calc(100%-80px)] w-full max-w-[400px] max-h-[500px] ml-auto mt-auto cursor-pointer relative">
               <div
                 id="draggable-h1"
                 className="absolute top-0 left-0 border-t-[20px] border-solid border-transparent w-full"
               />
+              <div className="hide-icon absolute right-1 w-6 h-5 cursor-pointer flex items-center">
+                <div className="line h-0.5 w-full bg-white"></div>
+              </div>
               <div className="inner w-full h-full border-t-[20px] border-solid border-primaryColor">
                 {!loaded ? (
                   <div className="loading absolute left-[50%] top-[40%] flex justify-center">
