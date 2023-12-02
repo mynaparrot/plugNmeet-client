16ce57c0abd9d3c63fc17f3b4a6fe87be4295b9b
Abdulkader69 <abdulkader.69k@gmail.com>
2023-12-02 23:40:05 +0600

    waiting room custom css

--- src/components/waiting-room/room-page/logo.tsx
+++ src/components/waiting-room/room-page/logo.tsx
@@ -5,17 +5,8 @@ const Logo = () => {
   const logo = useLogo('waiting-room');
 
   return (
-    <div
-      className="h-[90px] waiting-room-logo flex"
-      // style={{
-      //   backgroundImage: `url("${logo}")`,
-      // }}
-    >
-      <img
-        className="h-full w-full object-contain"
-        src={`${logo}`}
-        alt="logo"
-      />
+    <div className="waiting-room-logo">
+      <img className="waiting-room-logo-img" src={`${logo}`} alt="logo" />
     </div>
   );
 };
