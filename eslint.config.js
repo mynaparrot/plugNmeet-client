e00d8f5e156d218ee9c3af9c064ed7cce854ee4e
Jibon <jiboncosta57@gmail.com>
2024-10-14 16:48:36 +0200

    added `react-refresh`

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,50 @@
-const js = require('@eslint/js')
-const globals = require('globals')
-const reactHooks = require('eslint-plugin-react-hooks')
-const tseslint = require('typescript-eslint')
-const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended')
+const js = require('@eslint/js');
+const globals = require('globals');
+const reactHooks = require('eslint-plugin-react-hooks');
+const reactRefresh = require('eslint-plugin-react-refresh');
+const tsEslint = require('typescript-eslint');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
-module.exports = tseslint.config(
+module.exports = tsEslint.config(
   { ignores: ['dist'] },
   {
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommended,
-      eslintPluginPrettierRecommended
+      ...tsEslint.configs.recommended,
+      eslintPluginPrettierRecommended,
     ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser
+      globals: globals.browser,
+      parser: tsEslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname,
+      },
     },
     plugins: {
-      'react-hooks': reactHooks
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
       '@typescript-eslint/no-explicit-any': 'off',
+      'react-refresh/only-export-components': 'warn',
       'prettier/prettier': [
         'error',
         {
-          endOfLine: 'auto',
+          'singleQuote': true,
+          'semi': true,
+          'tabWidth': 2,
+          'bracketSpacing': true,
+          'trailingComma': 'all',
+          'arrowParens': 'always',
+          'endOfLine': 'auto',
         },
-      ]
+      ],
     },
   },
-)
+);
