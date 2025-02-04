a456f5f8aace439c0ce49ab01c72eff847b838af
Abdul Kader <abdulkader.69k@gmail.com>
2025-02-05 01:33:51 +0600

    Error page and Recorder Icon animation updated

--- src/components/extra-pages/Error.tsx
+++ src/components/extra-pages/Error.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import HeaderLogo from '../header/headerLogo';
 
 export interface IErrorPageProps {
   title: string;
@@ -14,12 +15,17 @@ const ErrorPage = ({ title, text }: IErrorPageProps) => {
         backgroundImage: `url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="100%25" height="100%25"%3E%3Cpattern id="bg" patternUnits="userSpaceOnUse" width="20" height="20"%3E%3Cg opacity="0.7"%3E%3Crect x="10" y="10" width="4" height="4" rx="2" fill="%23C2DAF2" /%3E%3C/g%3E%3C/pattern%3E%3Crect width="100%25" height="100%25" fill="url(%23bg)" /%3E%3C/svg%3E')`,
       }}
     >
-      <div className="content relative z-20 w-full max-w-[450px] flex items-center rounded-2xl border border-Gray-300 overflow-hidden bg-Gray-50 px-10 py-10">
-        <div className="inner">
-          <h2 className="text-2xl font-bold text-Gray-950 mb-6 leading-4">
+      <div className="content relative z-20 w-full max-w-xl flex items-center min-h-64 text-center rounded-2xl border border-Gray-300 overflow-hidden bg-Gray-50 px-10 py-10">
+        <div className="inner w-full">
+          <div className="logo flex justify-center mb-5">
+            <HeaderLogo />
+          </div>
+          <h2 className="text-2xl font-semibold text-Gray-950 mb-4 leading-4">
             {title}
           </h2>
-          <p className="break-words text-sm leading-5 text-Gray-700">{text}</p>
+          <p className="break-words text-base leading-5 text-Gray-950/90">
+            {text}
+          </p>
         </div>
       </div>
     </div>
