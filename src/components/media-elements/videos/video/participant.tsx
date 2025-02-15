e19105bb4c83cf98f1cc6bc35a4d6849de3801e8
Abdul Kader <abdulkader.69k@gmail.com>
2025-02-15 23:05:15 +0600

    design update

--- src/components/media-elements/videos/video/participant.tsx
+++ src/components/media-elements/videos/video/participant.tsx
@@ -16,7 +16,7 @@ const Participant = ({ userId, name, isLocal }: IParticipantProps) => {
   );
 
   return (
-    <div className="name absolute bottom-4 left-4 text-sm font-medium text-white z-10">
+    <div className="name absolute bottom-4 left-4 text-sm font-medium text-white z-10 flex items-center gap-2">
       {name} {isLocal ? '(me)' : null}
       {raisedHand ? <HandsIconSVG classes="h-4 w-auto" /> : null}
     </div>
