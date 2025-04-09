10d2b1d995a6072c336b8ae32801b10ccfd1dbd8
Abdul Kader <abdulkader.69k@gmail.com>
2025-04-09 12:21:59 +0600

    Fix: whiteboard width issue

--- src/components/polls/index.tsx
+++ src/components/polls/index.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-
 import Create from './create';
 import ListPolls from './listPolls';
-import { store } from '../../store';
+import { store, useAppDispatch } from '../../store';
+import { CloseIconSVG } from '../../assets/Icons/CloseIconSVG';
+import { updateIsActivePollsPanel } from '../../store/slices/bottomIconsActivitySlice';
 
 const PollsComponent = () => {
+  const dispatch = useAppDispatch();
+  const closePanel = () => {
+    dispatch(updateIsActivePollsPanel(false));
+  };
   const isAdmin = store.getState().session.currentUser?.metadata?.isAdmin;
   return (
-    <>
+    <div className="relative z-10 w-full bg-Gray-25 border-l border-Gray-200 h-full">
+      <div
+        className="hidden md:inline-block absolute z-50 right-3 3xl:right-5 top-[10px] 3xl:top-[18px] text-Gray-600 cursor-pointer"
+        onClick={closePanel}
+      >
+        <CloseIconSVG />
+      </div>
+      <div className="inner-wrapper relative z-20 w-full">
+        <div className="top flex items-center h-10 3xl:h-14 px-3 3xl:px-5 border-b border-Gray-200">
+          <p className="text-sm 3xl:text-base text-Gray-950 font-medium leading-tight">
+            Pools
+          </p>
+        </div>
+      </div>
       <ListPolls />
       {isAdmin ? <Create /> : null}
-    </>
+    </div>
   );
 };
 
