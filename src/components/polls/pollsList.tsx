fa565a77bda84e63f5200df7f5ff0cb03f738f0c
Jibon L. Costa <jiboncosta57@gmail.com>
2025-05-05 23:40:09 +0600

    clean up

--- src/components/polls/pollsList.tsx
+++ src/components/polls/pollsList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { PollInfo } from 'plugnmeet-protocol-js';
 
 import { useGetPollListsQuery } from '../../store/services/pollsApi';
@@ -9,11 +9,11 @@ const PollsList = () => {
   // const screenHeight = useAppSelector(
   //   (state) => state.bottomIconsActivity.screenHeight,
   // );
-  const { data, isLoading } = useGetPollListsQuery();
+  const { currentData: data, isFetching } = useGetPollListsQuery();
   // const isAdmin = store.getState().session.currentUser?.metadata?.isAdmin;
   const [polls, setPolls] = useState<PollInfo[]>([]);
 
-  useEffect(() => {
+  useMemo(() => {
     if (data && data.polls) {
       const sortedPolls = data.polls.slice();
       sortedPolls.sort((a, b) => Number(b.created) - Number(a.created));
@@ -30,7 +30,7 @@ const PollsList = () => {
         {polls.map((poll, index) => (
           <PollItem key={poll.id} item={poll} index={index} />
         ))}
-        {isLoading ? (
+        {isFetching ? (
           <div className="loading absolute text-center top-1/2 -translate-y-1/2 z-[999] left-0 right-0 m-auto">
             <div className="lds-ripple">
               <div className="border-secondaryColor" />
