cc951f1980ff7d2ef3e4a316ca0eb8105e48560c
Jibon L. Costa <jiboncosta57@gmail.com>
2025-05-05 22:54:29 +0600

    clean up

--- src/components/polls/pollItem/topMenu.tsx
+++ src/components/polls/pollItem/topMenu.tsx
@@ -55,73 +55,61 @@ const TopMenu = ({
   }, [closePollRes, t]);
 
   return (
-    <>
-      {isLoading ? (
-        <div className="loading absolute text-center top-1/2 -translate-y-1/2 z-[999] left-0 right-0 m-auto">
-          <div className="lds-ripple">
-            <div className="border-secondaryColor" />
-            <div className="border-secondaryColor" />
-          </div>
-        </div>
-      ) : null}
-      <Menu as="div">
-        {({ open }) => (
-          <>
-            <MenuButton className="relative flex-shrink-0 p-2 mr-2">
-              <div className="">
-                <FooterMenuIconSVG />
-              </div>
-            </MenuButton>
-            <Transition
-              as="div"
-              show={open}
-              enter="transition duration-100 ease-out"
-              enterFrom="transform scale-95 opacity-0"
-              enterTo="transform scale-100 opacity-100"
-              leave="transition duration-75 ease-out"
-              leaveFrom="transform scale-100 opacity-100"
-              leaveTo="transform scale-95 opacity-0"
+    <Menu as="div">
+      {({ open }) => (
+        <>
+          <MenuButton className="relative flex-shrink-0 p-2 mr-2">
+            <div className="">
+              <FooterMenuIconSVG />
+            </div>
+          </MenuButton>
+          <Transition
+            as="div"
+            show={open}
+            enter="transition duration-100 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
+          >
+            <MenuItems
+              static
+              className="origin-top-right z-20 absolute ltr:right-0 rtl:-left-4 mt-2 w-[244px] shadow-dropdownMenu rounded-[15px] overflow-hidden border border-Gray-100 bg-white p-2 ring-0 focus:outline-none"
             >
-              <MenuItems
-                static
-                className="origin-top-right z-20 absolute ltr:right-0 rtl:-left-4 mt-2 w-[244px] shadow-dropdownMenu rounded-[15px] overflow-hidden border border-Gray-100 bg-white p-2 ring-0 focus:outline-none"
-              >
+              <MenuItem>
+                <button
+                  className="h-9 3xl:h-11 w-full flex items-center bg-white hover:bg-Gray-50 text-sm 3xl:text-base gap-2 leading-none font-medium text-Gray-950 px-2 3xl:px-3 rounded-lg transition-all duration-300 relative"
+                  onClick={() => setViewDetails(true)}
+                >
+                  View details
+                </button>
+              </MenuItem>
+              <div className="divider h-1 w-[110%] bg-Gray-50 -ml-3 my-0.5"></div>
+              {isRunning ? (
+                <MenuItem>
+                  <button
+                    onClick={endPoll}
+                    className="h-9 3xl:h-11 w-full flex items-center bg-white hover:bg-Red-50 text-sm 3xl:text-base gap-2 leading-none font-medium text-Red-700 px-2 3xl:px-3 rounded-lg transition-all duration-300 relative"
+                  >
+                    {t('polls.end-poll')}
+                  </button>
+                </MenuItem>
+              ) : (
                 <MenuItem>
                   <button
                     className="h-9 3xl:h-11 w-full flex items-center bg-white hover:bg-Gray-50 text-sm 3xl:text-base gap-2 leading-none font-medium text-Gray-950 px-2 3xl:px-3 rounded-lg transition-all duration-300 relative"
-                    onClick={() => setViewDetails(true)}
+                    onClick={() => publishPollResultByChat(pollDataWithOption)}
                   >
-                    View details
+                    Publish result
                   </button>
                 </MenuItem>
-                <div className="divider h-1 w-[110%] bg-Gray-50 -ml-3 my-0.5"></div>
-                {isRunning ? (
-                  <MenuItem>
-                    <button
-                      onClick={endPoll}
-                      className="h-9 3xl:h-11 w-full flex items-center bg-white hover:bg-Red-50 text-sm 3xl:text-base gap-2 leading-none font-medium text-Red-700 px-2 3xl:px-3 rounded-lg transition-all duration-300 relative"
-                    >
-                      {t('polls.end-poll')}
-                    </button>
-                  </MenuItem>
-                ) : (
-                  <MenuItem>
-                    <button
-                      className="h-9 3xl:h-11 w-full flex items-center bg-white hover:bg-Gray-50 text-sm 3xl:text-base gap-2 leading-none font-medium text-Gray-950 px-2 3xl:px-3 rounded-lg transition-all duration-300 relative"
-                      onClick={() =>
-                        publishPollResultByChat(pollDataWithOption)
-                      }
-                    >
-                      Publish result
-                    </button>
-                  </MenuItem>
-                )}
-              </MenuItems>
-            </Transition>
-          </>
-        )}
-      </Menu>
-    </>
+              )}
+            </MenuItems>
+          </Transition>
+        </>
+      )}
+    </Menu>
   );
 };
 
