d6078b1de599b0160106ef7afc574fc552929049
Abdul Kader <abdulkader.69k@gmail.com>
2025-05-17 11:26:26 +0600

    percentage added for polls respons

--- src/components/polls/pollItem/details/respondents.tsx
+++ src/components/polls/pollItem/details/respondents.tsx
@@ -25,33 +25,38 @@ const Respondents = ({ pollDataWithOption }: RespondentsProps) => {
           {({ open }) => (
             <div className="bg-Gray-50 rounded-xl border border-gray-300 overflow-hidden w-[632px]">
               <DisclosureButton
-                className={`flex items-center justify-between gap-3 w-full px-[14px] bg-white h-9 rounded-xl  shadow-buttonShadow transition-all duration-300 ${open ? 'border-b border-gray-300' : ''}`}
+                className={`flex items-center justify-between gap-3 w-full pl-[14px] pr-2 bg-white h-9 rounded-xl  shadow-buttonShadow transition-all duration-300 ${open ? 'border-b border-gray-300' : ''}`}
               >
                 <span className="text-sm text-Gray-800">
                   {o.text} ({o.respondents.length})
                 </span>
-                <motion.div
-                  animate={{ rotate: open ? 180 : 0 }}
-                  transition={{ duration: 0.2 }}
-                  className=""
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="17"
-                    viewBox="0 0 16 17"
-                    fill="none"
+                <div className="right flex items-center gap-2">
+                  <span className="text-xs text-Gray-700">
+                    ({o.responsesPercentage + '%'})
+                  </span>
+                  <motion.div
+                    animate={{ rotate: open ? 180 : 0 }}
+                    transition={{ duration: 0.2 }}
+                    className=""
                   >
-                    <path d="M12 6.5L8 10.5L4 6.5" fill="#7493B3" />
-                    <path
-                      d="M12 6.5L8 10.5L4 6.5H12Z"
-                      stroke="#7493B3"
-                      strokeWidth="1.67"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </motion.div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="17"
+                      viewBox="0 0 16 17"
+                      fill="none"
+                    >
+                      <path d="M12 6.5L8 10.5L4 6.5" fill="#7493B3" />
+                      <path
+                        d="M12 6.5L8 10.5L4 6.5H12Z"
+                        stroke="#7493B3"
+                        strokeWidth="1.67"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </motion.div>
+                </div>
               </DisclosureButton>
 
               <AnimatePresence>
