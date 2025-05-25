832d1d73385ada4f5cc98bfd2ed4489d2a6ca772
Abdul Kader <abdulkader.69k@gmail.com>
2025-05-25 13:51:04 +0600

    Design: recorder modal updated

--- src/components/footer/icons/recording/recordingModal.tsx
+++ src/components/footer/icons/recording/recordingModal.tsx
@@ -4,11 +4,13 @@ import {
   DialogTitle,
   Transition,
   TransitionChild,
+  Button,
 } from '@headlessui/react';
 import { useTranslation } from 'react-i18next';
 import { RecordingFeatures } from 'plugnmeet-protocol-js';
 
 import { RecordingType } from './IRecording';
+import { PopupCloseSVGIcon } from '../../../../assets/Icons/PopupCloseSVGIcon';
 
 interface IRecordingModalProps {
   showModal: boolean;
@@ -43,28 +45,10 @@ const RecordingModal = ({
         <Transition appear show={showModal} as={Fragment}>
           <Dialog
             as="div"
-            className="recordingModal fixed inset-0 z-[9999] overflow-y-auto"
+            className="recordingModal fixed inset-0 w-screen overflow-y-auto z-10 bg-Gray-950/70"
             onClose={() => false}
           >
-            <div className="min-h-screen px-4 text-center">
-              <TransitionChild
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <div className="fixed inset-0 bg-black opacity-30" />
-              </TransitionChild>
-
-              <span
-                className="inline-block h-screen align-middle"
-                aria-hidden="true"
-              >
-                &#8203;
-              </span>
+            <div className="flex min-h-full items-center justify-center p-4">
               <TransitionChild
                 as={Fragment}
                 enter="ease-out duration-300"
@@ -74,8 +58,8 @@ const RecordingModal = ({
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <div className="inline-block w-full max-w-lg p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white dark:bg-darkPrimary shadow-xl rounded-2xl">
-                  <button
+                <div className="w-full max-w-lg bg-white border border-Gray-200 shadow-virtualPOP p-6 rounded-xl overflow-hidden duration-300 ease-out">
+                  {/* <button
                     className="close-btn absolute top-8 ltr:right-6 rtl:left-6 w-[25px] h-[25px] outline-none"
                     type="button"
                     onClick={() => closeModal()}
@@ -89,76 +73,91 @@ const RecordingModal = ({
                     className="text-lg font-medium leading-6 text-gray-900 dark:text-white ltr:text-left rtl:text-right mb-2"
                   >
                     {t('footer.icons.how-to-record')}
+                  </DialogTitle> */}
+                  <DialogTitle
+                    as="h3"
+                    className="flex items-center justify-between text-base 3xl:text-lg font-semibold leading-7 text-Gray-950 mb-2"
+                  >
+                    <span>{t('footer.icons.how-to-record')}</span>
+                    <Button onClick={() => closeModal()}>
+                      <PopupCloseSVGIcon classes="text-Gray-600" />
+                    </Button>
                   </DialogTitle>
                   <hr />
-                  <div className="mt-6">
+                  <div className="mt-4">
                     <form
                       action="#"
                       method="POST"
                       onSubmit={(e) => startRecording(e)}
                     >
-                      <div className="mb-10 pl-3">
-                        <p className="text-sm text-gray-500 dark:text-darkText">
+                      <div className="">
+                        <p className="text-sm text-Gray-950">
                           {t('footer.icons.recording-types-des')}
                         </p>
                         <div className="mt-4 pl-2 space-y-4">
                           {recordingFeatures?.isAllowLocal ? (
-                            <div className="flex items-center">
-                              <input
-                                type="radio"
-                                value="1"
-                                name="block"
-                                id="yes"
-                                checked={
-                                  recordingType ===
-                                  RecordingType.RECORDING_TYPE_LOCAL
-                                }
-                                onChange={() =>
-                                  setRecordingType(
-                                    RecordingType.RECORDING_TYPE_LOCAL,
-                                  )
-                                }
-                              />
-                              <label
-                                htmlFor="yes"
-                                className="ltr:ml-3 rtl:mr-3 block text-sm font-medium text-gray-700 dark:text-darkText"
-                              >
-                                {t('footer.icons.local-recording')}
-                              </label>
-                            </div>
+                            <>
+                              <div className="relative flex items-center overflow-hidden my-2">
+                                <input
+                                  type="radio"
+                                  value="1"
+                                  name="block"
+                                  id="yes"
+                                  checked={
+                                    recordingType ===
+                                    RecordingType.RECORDING_TYPE_LOCAL
+                                  }
+                                  onChange={() =>
+                                    setRecordingType(
+                                      RecordingType.RECORDING_TYPE_LOCAL,
+                                    )
+                                  }
+                                  className="polls-checkbox relative appearance-none w-[18px] h-[18px] border border-Gray-300 shadow-buttonShadow rounded-[6px] checked:bg-Blue2-500 checked:border-Blue2-600"
+                                />
+                                <label
+                                  className="text-sm text-Gray-900 absolute w-full h-full pl-7 z-10 flex items-center cursor-pointer"
+                                  htmlFor="yes"
+                                >
+                                  {t('footer.icons.local-recording')}
+                                </label>
+                              </div>
+                            </>
                           ) : null}
                           {recordingFeatures?.isAllowCloud ? (
-                            <div className="flex items-center">
-                              <input
-                                type="radio"
-                                value="0"
-                                name="block"
-                                id="no"
-                                checked={
-                                  recordingType ===
-                                  RecordingType.RECORDING_TYPE_CLOUD
-                                }
-                                onChange={() =>
-                                  setRecordingType(
-                                    RecordingType.RECORDING_TYPE_CLOUD,
-                                  )
-                                }
-                              />
-                              <label
-                                htmlFor="no"
-                                className="ltr:ml-3 rtl:mr-3 block text-sm font-medium text-gray-700 dark:text-darkText"
-                              >
-                                {t('footer.icons.cloud-recording')}
-                              </label>
-                            </div>
+                            <>
+                              <div className="relative flex items-center overflow-hidden my-2">
+                                <input
+                                  type="radio"
+                                  value="0"
+                                  name="block"
+                                  id="no"
+                                  checked={
+                                    recordingType ===
+                                    RecordingType.RECORDING_TYPE_CLOUD
+                                  }
+                                  onChange={() =>
+                                    setRecordingType(
+                                      RecordingType.RECORDING_TYPE_CLOUD,
+                                    )
+                                  }
+                                  className="polls-checkbox relative appearance-none w-[18px] h-[18px] border border-Gray-300 shadow-buttonShadow rounded-[6px] checked:bg-Blue2-500 checked:border-Blue2-600"
+                                />
+                                <label
+                                  className="text-sm text-Gray-900 absolute w-full h-full pl-7 z-10 flex items-center cursor-pointer"
+                                  htmlFor="no"
+                                >
+                                  {t('footer.icons.cloud-recording')}
+                                </label>
+                              </div>
+                            </>
                           ) : null}
                         </div>
                       </div>
 
-                      <div className="py-3 bg-gray-50 dark:bg-transparent text-right mt-4">
+                      <div className="mt-4">
                         <button
                           type="submit"
-                          className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-primaryColor hover:bg-secondaryColor focus:outline-none focus:ring-2 focus:ring-offset-2 focus:bg-secondaryColor"
+                          className="h-10 px-5 flex items-center ml-auto justify-center rounded-[15px] text-sm 3xl:text-base font-medium 3xl:font-semibold text-white bg-Blue border border-DarkBlue transition-all duration-300 hover:bg-DarkBlue shadow-buttonShadow"
                         >
                           {t('footer.icons.start-recording')}
                         </button>
